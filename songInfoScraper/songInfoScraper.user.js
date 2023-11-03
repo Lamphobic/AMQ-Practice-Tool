c8ec7af878c727ccb72393769b00777e93f61995
Xavier Lamphere <kongregamer@gmail.com>
2023-11-03 02:08:31 -0700

    Update songInfoScraper.user.js
    Use the websocket listener logically rather than the mess I had before. Now just yanks the info from the websocket directly.

--- songInfoScraper/songInfoScraper.user.js
+++ songInfoScraper/songInfoScraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Anime And Song Info Outputter
 // @namespace   https://github.com/Lamphobic/AMQ-Practice-Tool/raw/main/songInfoScraper/
-// @version      0.4
+// @version      0.5
 // @description  Prints anime info to console.
 // @author       Lamphobic
 // @match        https://animemusicquiz.com/*
@@ -25,140 +25,26 @@ function typeConverter(type) {
   return "Insert"
 }
 
-function songOutput(
-    songName,
-    artist,
-    type,
-    typeNumber,
-    urls,
-    siteIds,
-    vintage,
-    animeDifficulty,
-    altAnimeNames,
-    altAnimeNamesAnswers) {
-  console.log("Song name: ", songName);
-  console.log("Artist: ", artist);
-  console.log("Type: ", type);
-  console.log("Number: ", typeNumber);
-  console.log("Links: ", urls);
-  console.log("Ids: ", siteIds);
-  console.log("Year/Season: ", vintage);
-  console.log("Song Difficulty: ", animeDifficulty);
-  console.log("Alt Names: ", altAnimeNames);
-  console.log("Alt Name Answers: ", altAnimeNamesAnswers);
-}
-
-function onQuizReady(data) {
-  QuizInfoContainer.prototype.showInfo = function(
-    animeNames,
-    songName,
-    artist,
-    type,
-    typeNumber,
-    urls,
-    siteIds,
-    animeScore,
-    animeType,
-    vintage,
-    animeDifficulty,
-    animeTags,
-    animeGenre,
-    altAnimeNames,
-    altAnimeNamesAnswers
-  ) {
-    songOutput(
-      songName,
-      artist,
-      typeConverter(type),
-      typeNumber,
-      urls,
-      siteIds,
-      vintage,
-      animeDifficulty,
-      altAnimeNames,
-      altAnimeNamesAnswers
-    );
-    
-    let diffString = typeof animeDifficulty === "number" ? Math.round(animeDifficulty) : animeDifficulty;
-    this.$extraAnimeContent = $(
-      format(
-        this.EXTRA_INFO_TEMPLATE,
-        vintage,
-        animeScore,
-        animeType,
-        diffString,
-        animeTags.join(", "),
-        animeGenre.join(", ")
-      )
-    );
-    let animeName;
-    if (options.useRomajiNames) {
-      animeName = animeNames.romaji;
-    } else {
-      animeName = animeNames.english;
-    }
-
-    let altNames = [];
-    altAnimeNames.forEach((name) => {
-      if (animeName != name) {
-        altNames.push(name);
-      }
-    });
-
-    let altAnswers = [];
-    altAnimeNamesAnswers.forEach((answer) => {
-      if (animeName != answer && !altNames.includes(answer)) {
-        altAnswers.push(answer);
-      }
-    });
-
-    this.displayAnimeName(animeName, altNames, altAnswers);
-
-    this.$songName.text(songName);
-    this.$songArtist.text(artist);
-    this.$songType.text(convertTypeInfoToText(type, typeNumber));
-
-    let targetHost = quizVideoController.getCurrentHost();
-    let targetResolution = quizVideoController.getCurrentResolution();
-
-    let hostUrls;
-    if (urls[targetHost]) {
-      hostUrls = urls[targetHost];
-    } else {
-      console.log(
-        "debug: " +
-        JSON.stringify({
-          targetHost,
-          urls,
-        })
-      );
-      hostUrls = Object.values(urls)[0];
-    }
-
-    let sourceUrl;
-    if (hostUrls[targetResolution]) {
-      sourceUrl = hostUrls[targetResolution];
-    } else {
-      console.log(
-        "debug: " +
-        JSON.stringify({
-          targetResolution,
-          urls,
-        })
-      );
-      sourceUrl = Object.values(hostUrls)[0];
-    }
-
-    this.$songVideoLink.attr("href", sourceUrl);
-
-    this.$songAnimeLink.attr("href", generateAnimeSiteUrl(siteIds));
-
-    this.$songInfoLinkRow.removeClass('hideLinks');
-
-    this.showContent();
-  };
+function answersRecieved(result) {
+    let songInfo = result.songInfo;
+    let songName = songInfo.songName;
+    let artist = songInfo.artist;
+    let type = typeConverter(songInfo.type);
+    let number = songInfo.typeNumber;
+    let videoTargetMap = songInfo.videoTargetMap;
+    let siteIds = songInfo.siteIds;
+    let vintage = songInfo.vintage;
+    let difficulty = songInfo.animeDifficulty;
+    console.log("Song name: ", songName);
+    console.log("Artist: ", artist);
+    console.log("Type: ", type);
+    console.log("Number: ", number);
+    console.log("Links: ", videoTargetMap);
+    console.log("Ids: ", siteIds);
+    console.log("Year/Season: ", vintage);
+    console.log("Song Difficulty: ", difficulty);
 }
 
 function setup() {
-  new Listener("quiz ready", onQuizReady).bindListener();
+  new Listener("answer results", answersRecieved).bindListener();
 }
